f7de05f383f285c1c54fcfc2cf3cc7b87cd672e7
MatheusPMC <matheus.pmc@hotmail.com>
2021-10-13 19:43:34 -0300

    Atributos privados e encapsulamento

--- src/TestaMetodos.java
+++ src/TestaMetodos.java
@@ -1,13 +1,28 @@
 public class TestaMetodos {
 
     public static void main(String[] args) {
-        Conta conta = new Conta();
-        conta.saldo = 100;
-        conta.deposita(50);
-        System.out.println(conta.saldo);
-        conta.saca(20);
-        System.out.println(conta.saldo);
-        conta.saca(500);
-        System.out.println(conta.saldo);
+        Conta contaDoMatheus = new Conta();
+        contaDoMatheus.saldo = 100;
+        contaDoMatheus.deposita(50);
+        System.out.println(contaDoMatheus.saldo);
+        contaDoMatheus.saca(20);
+        System.out.println(contaDoMatheus.saldo);
+        contaDoMatheus.saca(500);
+        System.out.println(contaDoMatheus.saldo);
+
+        Conta contaDaMury = new Conta();
+        contaDaMury.deposita(1000);
+
+        boolean transferencia = contaDaMury.transfere(100, contaDoMatheus);
+        if (transferencia) {
+            System.out.println("transferencia com sucesso");
+            System.out.println(contaDaMury.saldo);
+            System.out.println(contaDoMatheus.saldo);
+        } else {
+            System.out.println("faltou dinheiro");
+        }
+        contaDoMatheus.titular = "Matheus Marques";
+        contaDaMury.titular = "Muryel Kanehira";
+
     }
 }
